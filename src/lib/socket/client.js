1559e5f65b977cab0f844dbd60392c9fc9061d4a
RayanXmonitoring <evaanjelinaaswitalumbanggaol@gmail.com>
2026-06-19 00:12:01 +0700

    Create client.js

---
+++ src/lib/socket/client.js
@@ -0,0 +1,71 @@
+import { io } from 'socket.io-client';
+
+class SocketClient {
+  constructor() {
+    this.socket = null;
+    this.isConnected = false;
+  }
+
+  connect(token) {
+    if (this.socket && this.isConnected) {
+      return this.socket;
+    }
+
+    const socketURL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+    this.socket = io(socketURL, {
+      auth: {
+        token: token,
+      },
+      transports: ['websocket', 'polling'],
+      reconnection: true,
+      reconnectionAttempts: 5,
+      reconnectionDelay: 1000,
+    });
+
+    this.socket.on('connect', () => {
+      this.isConnected = true;
+      console.log('Socket connected');
+    });
+
+    this.socket.on('disconnect', () => {
+      this.isConnected = false;
+      console.log('Socket disconnected');
+    });
+
+    this.socket.on('connect_error', (error) => {
+      console.error('Socket connection error:', error);
+    });
+
+    return this.socket;
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.isConnected = false;
+      this.socket = null;
+    }
+  }
+
+  emit(event, data) {
+    if (this.socket && this.isConnected) {
+      this.socket.emit(event, data);
+    } else {
+      console.warn('Socket not connected, event not sent:', event);
+    }
+  }
+
+  on(event, callback) {
+    if (this.socket) {
+      this.socket.on(event, callback);
+    }
+  }
+
+  off(event, callback) {
+    if (this.socket) {
+      this.socket.off(event, callback);
+    }
+  }
+}
+
+export default new SocketClient();
